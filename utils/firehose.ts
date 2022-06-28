9c2cd58beb529221375dd3402f4c3b8743682e28
tfrizza <tfrizza@gmail.com>
2022-06-28 22:05:59 +0800

    remove firehose list

--- utils/firehose.ts
+++ utils/firehose.ts
@@ -4,12 +4,6 @@ import { Pricing, Surface, Trade } from "./types";
 
 let firehose = new AWS.Firehose(AWSOptions);
 
-firehose.listDeliveryStreams(function (err, data) {
-  if (err) console.log(err, err.stack);
-  // an error occurred
-  else console.log(data); // successful response
-});
-
 export const putFirehoseBatch = (
   data: Trade[] | Pricing[] | Surface[],
   deliveryStreamName: string
