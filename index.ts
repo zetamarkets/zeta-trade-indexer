29867e4531f9fe413361d6ae076d18decd162778
Filip <filip@sierra.team>
2022-11-02 13:47:11 +1100

    initial commit

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Exchange, Network, utils, assets } from "@zetamarkets/sdk";
+import { Exchange, Network, utils, assets, constants } from "@zetamarkets/sdk";
 import { PublicKey, Connection } from "@solana/web3.js";
 import { collectMarketData } from "./event-queue-processing";
 import { FETCH_INTERVAL } from "./utils/constants";
@@ -11,6 +11,8 @@ const network =
     : process.env!.NETWORK === "devnet"
     ? Network.DEVNET
     : Network.LOCALNET;
+
+export let fetchingMarkets = new Map<assets.Asset, boolean[]>();
 
 export const loadExchange = async (
   allAssets: assets.Asset[],
@@ -30,6 +32,14 @@ export const loadExchange = async (
       undefined,
       undefined
     );
+
+    for (var asset of allAssets) {
+      fetchingMarkets.set(
+        asset,
+        new Array(constants.TOTAL_MARKETS).fill(false)
+      );
+    }
+
     alert(`${reload ? "Reloaded" : "Loaded"} exchange.`, false);
     // Close to reduce websocket strain
     await Exchange.close();
