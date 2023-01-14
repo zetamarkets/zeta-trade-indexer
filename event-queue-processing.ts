4dffe8abdb69615fbc6e4777826e23080f3b3f89
xjcaa <andrewchen67@gmail.com>
2023-01-14 18:10:08 +0800

    Add changes

--- event-queue-processing.ts
+++ event-queue-processing.ts
@@ -22,15 +22,7 @@ export async function collectMarketData(
   asset: assets.Asset,
   lastSeqNum?: Record<number, Record<number, number>>
 ) {
-  let accountInfo;
-  try {
-    accountInfo = await Exchange.connection.getAccountInfo(SYSVAR_CLOCK_PUBKEY);
-  } catch (e) {
-    alert(`Failed to get clock account info: ${e}`, true);
-    return;
-  }
-  let clockData = utils.getClockData(accountInfo);
-  let timestamp = clockData.timestamp;
+  let timestamp = Math.floor(Date.now() / 1000);
 
   await Promise.all(
     Exchange.getMarkets(asset).map(async (market) => {
