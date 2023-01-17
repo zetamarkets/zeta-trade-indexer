e719b8ee1934c06484fb81f6144fd18e18fb62ae
tfrizza <tfrizza@gmail.com>
2023-01-17 16:58:45 +0800

    vanilla winston logging

--- utils/logging.ts
+++ utils/logging.ts
@@ -1,8 +1,7 @@
 import * as winston from "winston";
-import ecsFormat from "@elastic/ecs-winston-format";
 
 export const logger = winston.createLogger({
   level: "info",
-  format: ecsFormat(),
+  format: winston.format.json(),
   transports: [new winston.transports.Console()],
 });
